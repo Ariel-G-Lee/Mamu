6562485137dadd4d97d9b9bbf54cc9d873e3f68e
heeejoo0518 <heeejoo0518@gmail.com>
2019-11-01 16:18:44 +0900

    주석 추가

--- app/src/main/java/com/example/mp_project/DBHandler.java
+++ app/src/main/java/com/example/mp_project/DBHandler.java
@@ -10,36 +10,54 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * DB 데이터 관리 클래스
+ * @author 김희주
+ */
 public class DBHandler {
     private DBHelper dbHelper;
     private SQLiteDatabase db;
     private String TableName;
 
+    //Constructor
     private DBHandler(Context context) throws SQLiteException {
         this.dbHelper = new DBHelper(context);
         this.db = dbHelper.getWritableDatabase();
         this.TableName = dbHelper.getTableName();
     }
 
+    //constructor 대신 이 메소드를 불러서 DBHandler을 사용함
     public static DBHandler open(Context context) throws SQLiteException{
         DBHandler dbHandler = new DBHandler(context);
         return dbHandler;
     }
 
+
+    //DB를 직접 닫지 않고 Helper 클래스만 close 함.
     public void close(){
         dbHelper.close();
     }
 
-    public int insert(ContentValues values){
-        //new memo insert
+    /**
+     * DB에 새로운 데이터를 insert함.
+     * 이 때, values에는 UseYN 정보와 PK가 포함될 필요 없고 내부적으로 처리된다.
+     * @param values (ContentValues - 여기서 키값은 어트리뷰트명과 동일)
+     * @return 성공: true, 실패: false
+     */
+    public boolean insert(ContentValues values){
         values.put(dbHelper.COLUMN_USEYN,"Y");
         if(db.insertOrThrow(TableName,null,values)<0){
-            //실패
-            return -1;
+            return false;
         }
-        return 0;
+        return true;
     }
 
+    /**
+     * key값을 이용해서 DB 데이터를 지움.
+     * 완전히 삭제하지 않고 UseYN의 값을 Y에서 N으로 바꾼다.
+     * @param key (int) - PK 값
+     * @return 성공: 삭제된 열의 번호, 실패: -1
+     */
     public int delete(int key){
         String where = dbHelper.COLUMN_ID + " = " + key;
         //db.delete(dbHelper.getTableName(),where, null); //완전삭제
@@ -50,18 +68,28 @@ public class DBHandler {
         return db.update(TableName, values, where, null); //간접삭제
     }
 
+    /**
+     * key값을 이용해서 DB 데이터를 업데이트.
+     * 한 번에 한 튜플만 수정할 수 있다.
+     * @param key (int) PK 값
+     * @param values (ContentValues- 여기서 키값은 어트리뷰트명과 동일) 업데이트할 정보들.
+     * @return 성공: 수정된 열의 번호, 실패: -1
+     */
     public int update(int key, ContentValues values){
         String where = dbHelper.COLUMN_ID + " = " + key;
         return db.update(TableName, values, where, null);
     }
 
+    /**
+     * 날짜정보를 이용해 해당 날짜의 메모를 전부 select
+     * @param date (String)
+     * @return 성공: ArrayList<Map<String,String>> , 실패: null
+     */
     public ArrayList<Map<String,String>> select(String date){
-        //해당 날짜의 메모 전부 select
         String selection = dbHelper.COLUMN_DATE+"=? AND "+dbHelper.COLUMN_USEYN +"=?";
         Cursor cursor = db.query(TableName, null, selection, new String[]{date,"Y"}, null, null, null);
 
         if(!cursor.moveToFirst()){
-            //해당 날짜 메모 없음
             return null;
         }
 
@@ -78,12 +106,35 @@ public class DBHandler {
             tmp.put("UseYN",cursor.getString(cursor.getColumnIndex(dbHelper.COLUMN_USEYN)));
 
             list.add(tmp);
-
             if(!cursor.moveToNext()) break;
         }
-
         return list;
+    }
 
+    /**
+     * key값을 이용해 해당 메모정보를 반환.
+     * @param key (String) PK값. Type에 유의! int 아님.
+     * @return Map<String,String> 메모 정보가 담겨져 있음. HashMap.
+     */
+    public Map<String,String> selectOne(String key){
+        String selection = dbHelper.COLUMN_ID+"=? AND "+dbHelper.COLUMN_USEYN +"=?";
+        Cursor cursor = db.query(TableName, null, selection, new String[]{key,"Y"}, null, null, null);
+
+        if(!cursor.moveToFirst()){
+            return null;
+        }
+
+        Map<String,String> resultMap= new HashMap<String,String>();
+
+        resultMap.put("Memo_ID",Integer.toString(cursor.getInt(cursor.getColumnIndex(dbHelper.COLUMN_ID))));
+        resultMap.put("CreationDate",cursor.getString(cursor.getColumnIndex(dbHelper.COLUMN_DATE)));
+        resultMap.put("MemoContents",cursor.getString(cursor.getColumnIndex(dbHelper.COLUMN_CONTENTS)));
+        resultMap.put("MemoTitle",cursor.getString(cursor.getColumnIndex(dbHelper.COLUMN_TITLE)));
+        resultMap.put("YoutubeUrl",cursor.getString(cursor.getColumnIndex(dbHelper.COLUMN_YTBURL)));
+        resultMap.put("ImagePath",cursor.getString(cursor.getColumnIndex(dbHelper.COLUMN_IMGPATH)));
+        resultMap.put("UseYN",cursor.getString(cursor.getColumnIndex(dbHelper.COLUMN_USEYN)));
+
+        return resultMap;
     }
 
 }
--- app/src/main/java/com/example/mp_project/DBHelper.java
+++ app/src/main/java/com/example/mp_project/DBHelper.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+/**
+ * DB 관리 클래스
+ * @author 김희주
+ */
 public class DBHelper extends SQLiteOpenHelper {
     private static final String DB_NAME = "mamu.db";
     private static final int DB_VERSION = 1;
@@ -16,11 +20,13 @@ public class DBHelper extends SQLiteOpenHelper {
     static final String COLUMN_IMGPATH = "ImagePath";
     static final String COLUMN_USEYN = "UseYN";
 
+    //Constructor
     public DBHelper(Context context){
         super(context,DB_NAME,null,DB_VERSION);
     }
+
+    //DB가 처음으로 만들어질 때 불리는 메소드.
     public void onCreate(SQLiteDatabase db){
-        //Called when the database is created for the first time.
         String sql = "CREATE TABLE " + TABLE_NAME +" ("
                 + COLUMN_ID +" INTEGER PRIMARY KEY AUTOINCREMENT,"
                 + COLUMN_DATE +" TEXT NOT NULL,"
@@ -34,14 +40,15 @@ public class DBHelper extends SQLiteOpenHelper {
         db.execSQL(sql);
 
     }
+
+    //DB가 이미 오픈되어 있을 때 불리는 메소드
     public void onOpen(SQLiteDatabase db){
-        //Called when the database has been opened.
         super.onOpen(db);
 
     }
 
+    //DB 업그레이드 메소드. 테이블을 삭제하고 새로 만들어서 데이터 전부 삭제
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-        //Called when the database needs to be upgraded.
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
