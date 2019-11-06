162f35f8bbe40c8678d4cdd8a0a2458ee84e1606
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2019-11-06 13:51:28 +0900

    플로팅버튼누르면
    MainActivity --> EditActivity & MemoActivity

--- app/src/main/java/com/example/mp_project/MainActivity.java
+++ app/src/main/java/com/example/mp_project/MainActivity.java
@@ -13,6 +13,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
     private ListView list;
     @Override
@@ -20,13 +22,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 맨 아래 핑크색 동그라미 버튼 (플로팅버튼) 눌렀을때 DetailActivity.class로 화면전환
-       /* FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, DetailActivity.class));
+               // startActivity(new Intent(MainActivity.this, DetailActivity.class));
+                Intent intent = new Intent(MainActivity.this, EditActivity.class);
+                intent.putExtra("key",1); //key값 전달 -1이면 생성, 그밖은 수정.
+                intent.putExtra("date","191103");
+                startActivity(intent);
             }
-        });*/
+        });
     }
 
 
