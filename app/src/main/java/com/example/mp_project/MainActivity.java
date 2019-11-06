1fe17337fca63498870ee08d5da940f8dacd6418
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2019-11-06 13:15:55 +0900

    MainActivity
    mainactivity 위에 TableLayout지우고 쓰면됨! -성주

--- app/src/main/java/com/example/mp_project/MainActivity.java
+++ app/src/main/java/com/example/mp_project/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.mp_project;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 맨 아래 핑크색 동그라미 버튼 (플로팅버튼) 눌렀을때 DetailActivity.class로 화면전환
+       /* FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, DetailActivity.class));
+            }
+        });*/
     }
+
+
 }
