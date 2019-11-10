0dda573472346e1d9ee34521180ab56428610977
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2019-11-10 12:05:13 +0900

    Handler안에 Iniaitlize 메소드추가
    플러스알파 listview화면안보이는거 수정

--- app/src/main/java/com/example/mp_project/MainActivity.java
+++ app/src/main/java/com/example/mp_project/MainActivity.java
@@ -29,7 +29,7 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private ListView list;
-    ArrayList<SampleData> DataList;
+    public static ArrayList<SampleData> DataList;
     int date;
     DBHandler handler;
 
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //어댑터랑 리스트뷰xml이랑 연결
-        this.InitializeMovieData();
+        this.InitializeData();
         ListView listView = (ListView)findViewById(R.id.listView);
         final MyAdapter myAdapter = new MyAdapter(this,DataList);
         listView.setAdapter(myAdapter);
@@ -55,13 +55,11 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               // startActivity(new Intent(MainActivity.this, DetailActivity.class));
                 Intent intent = new Intent(MainActivity.this, EditActivity.class);
                 intent.putExtra("key",1); //key값 전달 -1이면 생성, 그밖은 수정.
                 intent.putExtra("date","191103");
                 startActivity(intent);
             }
-
         });
 
         //리스트 나열된거 누르면 Activity Memo 로 이동......일단 토스처리해둠
@@ -88,27 +86,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     //데이터 초기화, 날짜에 맞는 데이터 불러와서 저장해주는곳
-    private void InitializeMovieData() {
+    private void InitializeData() {
+
         handler = DBHandler.open(this);
-
-        DataList = new ArrayList<SampleData>();
-        /*
-        movieDataList.add(new SampleData(R.drawable.movieposter1, "미션 이상임파서블","15세관람가"));
-        movieDataList.add(new SampleData(R.drawable.movieposter2, "아저씨","19세 이상관람가"));
-        movieDataList.add(new SampleData(R.drawable.movieposter3, "어벤져스","12세 이상관람가"));
-         */
-
-        DBHelper helper = new DBHelper(this);
-        SQLiteDatabase db = helper.getWritableDatabase();
-        Cursor cursor = db.rawQuery("select * from UserMemo where CreationDate="+date,null);
-
-        while (cursor.moveToNext()){
-            DataList.add(new SampleData(R.drawable.ic_add_black_24dp, cursor.getString(3),cursor.getString(2)));
-            // @@@@@@@@@@@R.Drawable 다음에 아이콘그림은 바꿔서 추가해주기 (icon)
-            // 한마디로 Date 값에 해당하는 데이터들을 무비데이터 List에 연속해서 집어넣어주는데
-            //getString 3번쨰열을 이름(name)에  2번째 열을 내용(content)에다 집어넣어준다
-            //이건 데이터에 이름이랑 내용이 세번쨰랑 두번쨰에 있어서
-        }
-
+        handler.InitializeData(date);
     }
+
+
 }
